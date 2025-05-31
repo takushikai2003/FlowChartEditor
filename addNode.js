a577131fd175ceef69138c28a66824a4ca4b7136
takushikai <takushikai@gmail.com>
2025-06-01 07:55:48 +0900

    FIX

--- addNode.js
+++ addNode.js
@@ -1,6 +1,10 @@
 import { state, offset } from "./var.js";
 import { renderLines } from "./renderLines.js";
 
+const FROM_LINE_COLOR = 'red';
+const TO_LINE_COLOR = 'blue';
+
+
 const canvas = document.getElementById('canvas');
 const lines = document.getElementById('lines');
 
@@ -50,18 +54,6 @@ export function addNode(type) {
         const toPoint = document.createElement('div');
         toPoint.className = 'to-point-decision';
         pointWrapper.appendChild(toPoint);
-        toPoint.addEventListener("mousedown",()=>{
-            connectTo = toPoint;
-            if (connectFrom) {
-                const from = connectFrom.dataset.id;
-                const to = toPoint.parentNode.parentNode.dataset.id;
-                if (!state.edges.find(e => e.from === from && e.to === to)) {
-                    state.edges.push({ from, to });
-                }
-                connectFrom = null;
-                renderLines();
-            }
-        });
 
 
         // YesとNoの文字
@@ -78,34 +70,19 @@ export function addNode(type) {
   else {
         shape.textContent = labels[type];
 
-        const pointWrapper = document.createElement('div');
-        pointWrapper.className = 'point-wrapper';
-        shape.appendChild(pointWrapper);
+        // const pointWrapper = document.createElement('div');
+        // pointWrapper.className = 'point-wrapper';
+        // shape.appendChild(pointWrapper);
 
         if(type == "start"){
             const fromPoint = document.createElement('div');
             fromPoint.className = 'from-point';
             shape.appendChild(fromPoint);
-            fromPoint.addEventListener("mousedown",()=> {
-                connectFrom = fromPoint;
-                console.log("connectFrom", connectFrom);
-            });
         }
         else if(type == "end"){
             const toPoint = document.createElement('div');
             toPoint.className = 'to-point';
             shape.appendChild(toPoint);
-            toPoint.addEventListener("mousedown",()=>{
-                connectTo = toPoint;
-                if (connectFrom) {
-                    console.log("connectTo", connectTo);
-                    const from = connectFrom.dataset.id;
-                    const to = toPoint.parentNode.parentNode.dataset.id;
-                    renderLines();
-                    connectFrom = null;
-                    connectTo = null;
-                }
-            });
         }
         else{
             const fromPoint = document.createElement('div');
@@ -139,7 +116,93 @@ function bindNodeEvents(node) {
 
     console.log(state);
 
-    // if(node.)
+    const fromPoint = node.querySelector('.from-point');
+    const toPoint = node.querySelector('.to-point');
+    const fromPointNo = node.querySelector('.from-point-no');
+    const fromPointYes = node.querySelector('.from-point-yes');
+    const toPointDecision = node.querySelector('.to-point-decision');
+
+    if (fromPoint) {
+        fromPoint.onmousedown = () => {
+            connectFrom = fromPoint;
+            console.log("connectFrom", connectFrom);
+        };
+        fromPoint.onmouseover = () => {
+            console.log("over fromPoint");
+            fromPoint.style.backgroundColor = FROM_LINE_COLOR;
+        }
+        fromPoint.onmouseleave = () => {
+            fromPoint.style.backgroundColor = '';
+        }
+    }
+    if (toPoint) {
+        toPoint.onmousedown = () => {
+            connectTo = toPoint;
+            if (connectFrom) {
+                const from = connectFrom.dataset.id;
+                const to = toPoint.parentNode.parentNode.dataset.id;
+                if (!state.edges.find(e => e.from === from && e.to === to)) {
+                    state.edges.push({ from, to });
+                }
+                connectFrom = null;
+                connectTo = null;
+                renderLines();
+            }
+        };
+
+        toPoint.onmouseover = () => {
+            if(!connectFrom) return;
+            toPoint.style.backgroundColor = TO_LINE_COLOR;
+        }
+        toPoint.onmouseleave = () => {
+            toPoint.style.backgroundColor = '';
+        }
+    }
+    if (fromPointNo) {
+        fromPointNo.onmousedown = () => {
+            connectFrom = fromPointNo;
+        };
+        fromPointNo.onmouseover = () => {
+            fromPointNo.style.backgroundColor = FROM_LINE_COLOR;
+        }
+        fromPointNo.onmouseleave = () => {
+            fromPointNo.style.backgroundColor = '';
+        }
+    }
+    if (fromPointYes) {
+        fromPointYes.onmousedown = () => {
+            connectFrom = fromPointYes;
+        };
+        fromPointYes.onmouseover = () => {
+            fromPointYes.style.backgroundColor = FROM_LINE_COLOR;
+        }
+        fromPointYes.onmouseleave = () => {
+            fromPointYes.style.backgroundColor = '';
+        }
+    }
+    if (toPointDecision) {
+        toPointDecision.onmousedown = () => {
+            connectTo = toPointDecision;
+            if (connectFrom) {
+                const from = connectFrom.dataset.id;
+                const to = toPointDecision.parentNode.parentNode.dataset.id;
+                if (!state.edges.find(e => e.from === from && e.to === to)) {
+                    state.edges.push({ from, to });
+                }
+                connectFrom = null;
+                connectTo = null;
+                renderLines();
+            }
+        };
+        toPointDecision.onmouseover = () => {
+            if(!connectFrom) return;
+            toPointDecision.style.backgroundColor = TO_LINE_COLOR;
+        }
+        toPointDecision.onmouseleave = () => {
+            toPointDecision.style.backgroundColor = '';
+        }
+    }
+
 
     // div.onmouseup = e => {
     //     if (connectFrom && connectFrom !== div) {
