8cbd8c8602eb889ad9c37a5fd98e56e46a9b0083
takushikai <takushikai@gmail.com>
2025-06-04 02:03:11 +0900

    一旦、一重ループは動く

--- runtime/runtime.js
+++ runtime/runtime.js
@@ -6,7 +6,7 @@ export function run() {
     // state.nodesはfnの情報を持っているので、そこから必要な情報を取得する
     const state_copy = JSON.parse(JSON.stringify(state));
 
-    const loop_stack = [];
+    const loop_stacks = [];
 
     const startNode = state_copy.nodes.find(node => node.type === 'start');
     const startNodeEl = document.querySelector(`.node[data-id="${startNode.id}"]`);
@@ -19,19 +19,20 @@ export function run() {
     function eatToken(fromId){
         const edge = state_copy.edges.find(edge => edge.from === fromId);
         if(!edge) return null;
+
+        // // そのエッジを消す
         const edgeIndex = state_copy.edges.indexOf(edge);
-        // そのエッジを消す
-        if(edgeIndex !== -1) {
-            state_copy.edges.splice(edgeIndex, 1);
-        }
+        // if(edgeIndex !== -1) {
+        //     state_copy.edges.splice(edgeIndex, 1);
+        // }
 
         // console.log(edge.to);
-        const nextNodeDom = getNodeDomBytoId(edge.to); // そのエッジのtoポイントを探す
+        const nowNodeDom = getNodeDomByfromId(fromId); // そのエッジのfromポイントを探す
+        const nowNode = state.nodes.find(node => node.id === nowNodeDom.dataset.id);
 
-        code += `${nextNodeDom.dataset.type}\n`;
-        // console.log(nextNodeDom.dataset.type);
-        // ノードに紐づけられた内容を表示
+        const nextNodeDom = getNodeDomBytoId(edge.to); // そのエッジのtoポイントを探す
         const nextNode = state.nodes.find(node => node.id === nextNodeDom.dataset.id);
+        code += `${nextNodeDom.dataset.type}\n`;
 
         if(nextNode.type === 'start'){
             console.error("Start node reached, this should not happen.");
@@ -42,11 +43,11 @@ export function run() {
             console.log("実行が完了しました。");
             return true;
         }
-        else if(nextNode.type === 'io') {
-            // 入出力ノードの場合は、何もしない
-            const fromId = nextNodeDom.querySelector('.from-point').dataset.id;
-            eatToken(fromId);
-        }
+        // else if(nextNode.type === 'io') {
+        //     // 入出力ノードの場合は、何もしない
+        //     const fromId = nextNodeDom.querySelector('.from-point').dataset.id;
+        //     eatToken(fromId);
+        // }
         else if(nextNode.type === 'process') {
             // 処理ノードの場合は、fnを実行する
             const fromId = nextNodeDom.querySelector('.from-point').dataset.id;
@@ -69,20 +70,46 @@ export function run() {
             eatToken(fromId);
         }
         else if(nextNode.type === 'loop_start') {
-            // ループ開始ノードの場合は、ループスタックに追加
-            loop_stack.push(nextNode);
+
             const fromId = nextNodeDom.querySelector('.from-point').dataset.id;
+
+
+            // ループ開始ノードの場合は、ループスタックに追加
+            // ループ回数-1回分追加する（一回分はすぐに実行されるため）
+            const loop_stack = [];
+            for(let i = 0; i < nextNode.data.loop_count -1; i++){
+                // ループスタックに追加
+                console.log("loop push", fromId);
+                loop_stack.push(fromId);
+            }
+
+            loop_stacks.push(loop_stack);
+            
             eatToken(fromId);
         }
         else if(nextNode.type === 'loop_end') {
             // ループ終了ノードの場合は、ループスタックからポップして、ループ開始ノードに戻る
-            if(loop_stack.length > 0) {
-                const loopStartNode = loop_stack.pop();
-                const fromId = loopStartNode.el.querySelector('.from-point').dataset.id;
+            if(loop_stacks.length > 0) {
+                console.log("loop pop")
+                const loop_stack = loop_stacks.pop();
+                // ループスタックが空でない場合は、最後のfromIdを取得してループ開始ノードに戻る
+                const fromId = loop_stack.pop();
+
+                // ループスタックがまだ残っている場合は戻す
+                if(loop_stack.length > 0) {
+                    console.log("loop stack not empty, continue loop");
+                    loop_stacks.push(loop_stack);
+                }
                 eatToken(fromId);
-            } else {
-                console.error("Loop end reached without a matching loop start.");
             }
+            // ループスタックが空の場合は、ループを終了して次のノードに進む
+            else{
+                const fromId = nextNodeDom.querySelector('.from-point').dataset.id;
+                eatToken(fromId);
+            }
+            // else {
+            //     console.error("Loop end reached without a matching loop start.");
+            // }
         }
         else if(nextNode.type === 'decision') {
             // 分岐ノードの場合は、yes/noのエッジを処理する
@@ -136,4 +163,12 @@ function getNodeDomBytoId(toId){
     const toPoint = document.querySelector(`[data-id="${toId}"]`);
 
     return toPoint.closest('.node');
+}
+
+
+function getNodeDomByfromId(from){
+    // そのエッジのfromポイントを探す
+    const fromPoint = document.querySelector(`[data-id="${from}"]`);
+
+    return fromPoint.closest('.node');
 }
