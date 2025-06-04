8069657a81dec1358f6aaf1e63abfec0afd1780e
takushikai <takushikai@gmail.com>
2025-06-04 11:39:29 +0900

    fix

--- data/question_3.js
+++ data/question_3.js
@@ -1,47 +1,19 @@
 // 1,1から4,4までの階段
 export const walls = [
     [true, true, true, true, true, true, true],
-    [true, false, true, false, true, true, true],
-    [true, false, false, false, false, false, true],
-    [true, false, true, false, true, false, true],
-    [true, true, true, true, false, false, true],
-    [true, true, true, true, true, false, true],
-    [true, true, true, true, true, true, true]
-];//7*7
-
-const maze1 = [
-    [true, true, true, true, true, true, true],
-    [true, false, false, false, false, false, true],
-    [true, false, false, false, true, true, true],
-    [true, true, true, false, false, false, true],
-    [true, false, false, false, false, false, true],
-    [true, false, false, false, false, false, true],
-    [true, true, true, true, true, true, true]
-];//7*7
-
-const maze2 = [
-    [true, true, true, true, true, true, true],
-    [true, false, true, false, true, true, true],
-    [true, false, false, false, false, false, true],
-    [true, false, true, false, true, false, true],
-    [true, true, true, true, false, false, true],
-    [true, true, true, true, true, false, true],
-    [true, true, true, true, true, true, true]
-];
-const maze3 = [
-    [true, true, true, true, true, true, true],
-    [true, false, true, false, true, false, true],
     [true, false, true, false, false, false, true],
+    [true, false, true, false, true, false, true],
     [true, false, false, false, true, false, true],
     [true, true, true, true, true, false, true],
-    [true, true, true, true, true, false, true],
+    [true, false, false, false, false, false, true],
     [true, true, true, true, true, true, true]
-];
+];//7*7
 
 
-export const isMaze = true; // 迷路かどうか
 
-export const mazes = [maze1, maze2, maze3];
+export const isMaze = false; // 迷路かどうか
+
+export const mazes = [];
 
 export const start = { x: 1, y: 1 }; // スタート位置
 export const goal = { x: 5, y: 5 }; // ゴール位置
