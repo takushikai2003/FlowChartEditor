6dc00fe23b6fcf3749a378e16308250b59147f45
takushikai <takushikai@gmail.com>
2025-06-04 10:51:07 +0900

    update

--- data/question_3.js
+++ data/question_3.js
@@ -1,9 +1,9 @@
 // 1,1から4,4までの階段
 export const walls = [
     [true, true, true, true, true, true, true],
+    [true, false, true, false, true, true, true],
+    [true, false, false, false, false, false, true],
     [true, false, true, false, true, false, true],
-    [true, false, true, false, false, false, true],
-    [true, false, false, false, true, false, true],
     [true, true, true, true, false, false, true],
     [true, true, true, true, true, false, true],
     [true, true, true, true, true, true, true]
@@ -22,7 +22,7 @@ const maze1 = [
 const maze2 = [
     [true, true, true, true, true, true, true],
     [true, false, true, false, true, true, true],
-    [true, false, true, false, false, false, true],
+    [true, false, false, false, false, false, true],
     [true, false, true, false, true, false, true],
     [true, true, true, true, false, false, true],
     [true, true, true, true, true, false, true],
